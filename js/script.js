2404ed100a33b3f2e9267b51750ecab3b3c3be9f
ola <ola@example.com>
2022-12-30 14:50:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1,
+            dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
